0b95bfa7f7b5061f7215df0f901370c1d46605e4
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2024-07-27 16:00:21 +0500

    Working on Single Product page

--- app/singleProduct/page.js
+++ app/singleProduct/page.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Header from '../Components/Header'
+import React from "react";
+import Header from "../Components/Header";
+import ProductPage from "../Components/ProductPage";
+import Banner from "../Components/Banner";
+import Image from "next/image";
+import ArrowLeft from "@/public/images/Arrow-left.svg";
+import Link from "next/link";
 
 const page = () => {
   return (
     <>
-    <Header/>
-    
-    </>
-  )
-}
+      <Header />
+      <ProductPage />
+      <div className="xl:mx-[90px] mt-10">
+        <Link href="/product" className="inline-flex items-center gap-3">
+          <Image src={ArrowLeft} />
+          <span className="text-neutral-800 text-base font-medium font-['Poppins'] leading-normal">
+            Back
+          </span>
+        </Link>
+      </div>
 
-export default page
+      <Banner />
+    </>
+  );
+};
+
+export default page;
