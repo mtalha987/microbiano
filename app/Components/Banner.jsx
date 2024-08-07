cbc1713dc8a2c83d4cfd23bf5d711f555d767ba0
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2024-08-07 18:28:35 +0500

    Banner Img Fixed

--- app/Components/Banner.jsx
+++ app/Components/Banner.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import Image from "next/image";
-import BannerImg from "@/public/images/bannericon.png";
+import BannerImg from "@/public/images/BannerCut.png";
 import Link from "next/link";
 
 const Banner = () => {
   return (
     <>
       <div className="lg:mx-[90px]  lg:mt-[124px] mt-6 xs:mt-0 flex">
-        {/* <div className=""> */}
-          {/* <Image src={BannerImg} /> */}
           <div className="w-[526px] h-[393px] relative hidden lg:block">
             <Image
-              className="w-[809px] h-[524px] left-[0px] top-[-89px] absolute origin-top-left"
+              className="w-[809px] h-[524px] left-[0px] top-[-89px] absolute origin-top-left ml-14"
               src={BannerImg} />
-            
           </div>
-        {/* </div> */}
         <div className="w-full h-[318px] border border-white bg-[#9e1f66] flex flex-col xs:gap-4 md:pt-14 pt-4 md:pl-32 pl-4  ">
           <h2 className=" text-white text-[42px] font-normal font-['Arial']">
             Enjoy up to 10% off!!
