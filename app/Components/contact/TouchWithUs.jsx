3952904adcfd744fa5a9d5df38d0f54ed2dc6634
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2024-08-12 12:27:59 +0500

    About and Contact us page complete

--- app/Components/contact/TouchWithUs.jsx
+++ app/Components/contact/TouchWithUs.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import TouchContact from "@/public/images/touchContact.svg";
+// import TouchContact from "@/public/images/touchContact.svg";
 
 const TouchWithUs = () => {
   return (
     <>
-      <div className="xl:px-[90px] lg:px-[40px] px-5 flex mt-20 bg-white lg:justify-between flex-wrap justify-center ">
-        <div className="py-20 lg:pl-14 xs:pl-3 pl-5 shadow-lg lg:w-6/12">
+      <div className="xl:px-[90px] lg:px-[40px] px-5 flex sm:mt-20 xs:mt-10 bg-white lg:justify-between flex-wrap justify-center  ">
+        <div className="lg:py-20 py-10 lg:pl-14 xs:pl-3 pl-5 shadow-lg lg:w-6/12">
           <h2 className="text-black text-[32px] font-normal font-nohemi">
             Get In Touch With Us
           </h2>
@@ -18,14 +18,14 @@ const TouchWithUs = () => {
             <input
               type="text"
               placeholder="Name"
-              className="py-3 pl-5 w-[80%] bg-[#FAFAFA] font-primary leading-[17.68px] font-normal text-base text-[#04040466]"
+              className="py-3 pl-5 w-[80%] xs:w-[92%] bg-[#FAFAFA] font-primary leading-[17.68px] font-normal text-base text-[#04040466]"
             />
             <input
               type="email"
               name=""
               id=""
               placeholder="Email"
-              className="py-3 pl-5  w-[80%] bg-[#FAFAFA] font-primary leading-[17.68px] font-normal text-base text-[#04040466] mt-5"
+              className="py-3 pl-5  w-[80%] xs:w-[92%] bg-[#FAFAFA] font-primary leading-[17.68px] font-normal text-base text-[#04040466] mt-5"
             />
             <textarea
               name=""
@@ -33,17 +33,25 @@ const TouchWithUs = () => {
               cols="30"
               rows="7"
               placeholder="Message"
-              className="py-3 pl-5  w-[80%] bg-[#FAFAFA] font-primary leading-[17.68px] font-normal text-base text-[#04040466] mt-5"
+              className="py-3 pl-5  w-[80%] xs:w-[92%] bg-[#FAFAFA] font-primary leading-[17.68px] font-normal text-base text-[#04040466] mt-5"
             ></textarea>
             <div>
-              <button className="bg-primary text-white py-3 px-10 mt-5">
+              <button className="bg-secondary text-white py-3 px-10 mt-5">
                 Submit
               </button>
             </div>
           </form>
         </div>
-        <div className="lg:w-6/12 flex justify-end mt-12 lg:mt-0">
-          <Image src={TouchContact} alt="Contact Us" />
+        <div className="lg:w-6/12 flex justify-end mt-12 lg:mt-0 h-[400px] w-[100%] lg:h-auto  lg:px-0 xs:px-3 px-5">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14495.85232539756!2d54.7278259!3d24.7281477!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5effd1737a9a77%3A0x4bdf4e512ac74a2a!2sSafe%20Care%20Medical%20Industry!5e0!3m2!1sen!2s!4v1723027491491!5m2!1sen!2s"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            // allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            allowFullScreen />
         </div>
       </div>
     </>
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import PageNameBanner from '../Components/PageNameBanner'
-import AboutHeroImg from '@/public/images/aboutHero.svg';
-import TouchWithUs from '../Components/contact/TouchWithUs';
-import LocationMap from '../Components/contact/LocationMap';
-import ProductPageBanner from "@/public/images/productPageBanner.png"
-
+import Hero from '../Components/contact/Hero'
+import TouchWithUs from '../Components/contact/TouchwithUs'
+import ContactInformation from '../Components/contact/ContactInformation'
 
 const page = () => {
   return (
     <>
-    <PageNameBanner title="Contact Us" image={ProductPageBanner}/>
-    <TouchWithUs/>
-    <LocationMap/>
+      <Hero/>
+      <TouchWithUs/>
+      <ContactInformation/>
     </>
   )
 }
