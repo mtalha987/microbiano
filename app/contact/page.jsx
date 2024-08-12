6f73ff488c7a00e28f7308fb8af990485cb673b2
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2024-08-12 13:15:27 +0500

    Contact Page Done

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Hero from '../Components/contact/Hero'
-import TouchWithUs from '../Components/contact/TouchwithUs.jsx'
 import ContactInformation from '../Components/contact/ContactInformation'
 import PageNameBanner from '../Components/PageNameBanner'
 import ProductPageBanner from "@/public/images/productPageBanner.png"
+import TouchWithUs from '../Components/contact/TouchwithUs'
 
 const page = () => {
   return (
